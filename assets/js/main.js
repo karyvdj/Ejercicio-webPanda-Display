5830d8b4cc4afcd0b6cbc5025eb4b41bd9a5dbe4
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-18 10:44:18 -0500

    borrar fotos en proceso

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // alert("hola")
 var botonExtincion = document.getElementById('extincion');
 var botonFotos = document.getElementById('fotos');
+var fotos = document.getElementsByClassName('imagenBoton');
 
 var p02 = document.getElementById('p02');
+var borrar = document.getElementsByClassName('cerrar');
 
 function quitarP(){
   p02.style.display = "none";
@@ -14,3 +16,13 @@ function ponerP(){
 
 botonExtincion.addEventListener("click", quitarP);
 botonExtincion.addEventListener("dblclick", ponerP);
+
+for (var i = 0; i < fotos.length; i++) {
+  fotos[i].addEventListener("click", cerrar);
+}
+
+function cerrar() {
+  this.style.display = "none";
+}
+
+borrar.addEventListener("click", cerrar);
