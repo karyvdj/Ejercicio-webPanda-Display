50f13714e52d6957f42e4bd30d3f56de039f5359
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-18 18:20:26 -0500

    Funcionan los botones completos y las imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,60 @@
 // alert("hola")
+var botonOrigen = document.getElementById('origen');
 var botonExtincion = document.getElementById('extincion');
 var botonFotos = document.getElementById('fotos');
 var fotos = document.getElementsByClassName('imagenBoton');
 
+var p01 = document.getElementById('p01');
 var p02 = document.getElementById('p02');
 var borrar = document.getElementsByClassName('cerrar');
 
-function quitarP(){
-  p02.style.display = "none";
+function quitarP1(){
+  p01.style.visibility = "hidden";
+  botonOrigen.removeEventListener("click", quitarP1);
+  botonOrigen.addEventListener("click", ponerP1);
 }
 
-function ponerP(){
-  p02.style.display = "inline-block";
+function ponerP1(){
+  p01.style.visibility = "visible";
+  botonOrigen.removeEventListener("click", ponerP1);
+  botonOrigen.addEventListener("click", quitarP1);
 }
 
-botonExtincion.addEventListener("click", quitarP);
-botonExtincion.addEventListener("dblclick", ponerP);
+botonOrigen.addEventListener("click", quitarP1);
 
-for (var i = 0; i < fotos.length; i++) {
-  fotos[i].addEventListener("click", cerrar);
+
+function quitarP2(){
+  p02.style.visibility = "hidden";
+  botonExtincion.removeEventListener("click",quitarP2);
+  botonExtincion.addEventListener("click", ponerP2);
 }
 
-function cerrar() {
-  this.style.display = "none";
+function ponerP2(){
+  p02.style.visibility = "visible";
+  botonExtincion.removeEventListener("click",ponerP2);
+  botonExtincion.addEventListener("click", quitarP2);
 }
 
+botonExtincion.addEventListener("click", quitarP2);
+
+
+function identificarTache() {
+  cerrar(this);
+}
+
+for (var i = 0; i < borrar.length; i++) {
+  borrar[i].addEventListener("click", identificarTache);
+}
+
+function cerrar(tache) {
+  var padre = tache.parentNode;
+  padre.style.display = "none";
+}
+
+function restaurarFotos() {
+  for (var i = 0; i < fotos.length; i++) {
+    fotos[i].style.display = "inline-block"
+  }
+}
+botonFotos.addEventListener("click", restaurarFotos)
 // borrar.addEventListener("click", cerrar);
