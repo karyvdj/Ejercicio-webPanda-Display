affa8b044adaa9fd3dd7edda1295fa4691989c2f
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-18 10:06:25 -0500

    funcion desaparecer parrafo origen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-// alert("hola");
+// alert("hola")
+var botonOrigen = document.getElementById('origen');
+var botonExtincion = document.getElementById('extincion');
+var botonFotos = document.getElementById('fotos');
+
+var p01 = document.getElementById('p01');
+
+function quitarP(){
+  p01.style.display = "none";
+}
+
+function ponerP(){
+  p01.style.display = "block";
+}
+
+botonOrigen.addEventListener("click", quitarP);
+botonOrigen.addEventListener("click", quitarP);
